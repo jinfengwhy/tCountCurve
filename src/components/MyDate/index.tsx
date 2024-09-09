21ef7410926570119219858fe4eacbb4bff19508
jinfengwhy <xingzhe290@foxmail.com>
2024-09-09 17:11:26 +0800

    首页新增日期显示

---
+++ src/components/MyDate/index.tsx
@@ -0,0 +1,28 @@
+import { memo, useEffect, useState } from "react";
+
+import { View, Text } from "@tarojs/components";
+
+import './index.less';
+
+const DateComponent = memo(() => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // 添加空依赖数组以确保 useEffect 只在组件挂载和卸载时运行
+
+  return (
+    <View className="components-my-date">
+      <Text className="date">{ date.toLocaleDateString(undefined, { year: 'numeric', month: '2-digit', day: '2-digit' })}</Text>
+      <Text>{ date.toLocaleTimeString() }</Text>
+    </View>
+  );
+});
+
+export default DateComponent;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 
 import { View, Button, Text } from '@tarojs/components'
+import MyDate from '../../components/MyDate/index';
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
@@ -28,7 +29,8 @@ function Index (props: IProps) {
   const { counter, add, dec, asyncAdd } = props;
 
   return (
-    <View className='index'>
+    <View className='pages-index'>
+      <MyDate />
       <Button className='add_btn' onClick={add}>+</Button>
       <Button className='dec_btn' onClick={dec}>-</Button>
       <Button className='dec_btn' onClick={asyncAdd}>async</Button>
