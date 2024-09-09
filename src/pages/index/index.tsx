4c6f38c3184d52f85db0afafa27fe793b42dddeb
jinfengwhy <xingzhe290@foxmail.com>
2024-09-09 17:06:27 +0800

    首页改为函数式组件

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,20 +1,10 @@
-import { Component, PropsWithChildren } from 'react'
 import { connect } from 'react-redux'
+
 import { View, Button, Text } from '@tarojs/components'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.less'
-
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
 
 type PageStateProps = {
   counter: {
@@ -28,52 +18,34 @@ type PageDispatchProps = {
   asyncAdd: () => any
 }
 
-type PageOwnProps = {}
-
-type PageState = {}
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+type IProps = PageStateProps & PageDispatchProps
 
 interface Index {
   props: IProps;
 }
 
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
-class Index extends Component<PropsWithChildren> {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
+function Index (props: IProps) {
+  const { counter, add, dec, asyncAdd } = props;
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
-      </View>
-    )
-  }
+  return (
+    <View className='index'>
+      <Button className='add_btn' onClick={add}>+</Button>
+      <Button className='dec_btn' onClick={dec}>-</Button>
+      <Button className='dec_btn' onClick={asyncAdd}>async</Button>
+      <View><Text>{counter.num}</Text></View>
+      <View><Text>Hello, World</Text></View>
+    </View>
+  )
 }
 
-export default Index
+const mapStateToProps = (state: PageStateProps) => ({
+  counter: state.counter,
+})
 
+const mapDispatchToProps = (dispatch: any) => ({
+  add: () => dispatch(add()),
+  dec: () => dispatch(minus()),
+  asyncAdd: () => dispatch(asyncAdd()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
